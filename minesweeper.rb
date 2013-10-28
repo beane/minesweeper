293506f2f3c63554b9cdc9a49bcce8ef51002dfd
Nick Willett-Jeffries & Brian Deane <brian+nick@appacademy.io>
2013-10-28 13:22:44 -0400

    Add reveal function
    To uncover board squares based on a given coordinate guess.
    Strong candidate for refactoring...

--- minesweeper.rb
+++ minesweeper.rb
@@ -14,13 +14,52 @@ class Board
   ]
 
   BOMB = "B"
-  EMPTY = "*"
+  UNEXPLORED = "*"
+  NO_BOMBS = '_'
 
   def initialize
-    @board = Array.new(9) {EMPTY * 9}
-    @hidden_board = Array.new(9) {EMPTY * 9}
+    @board = Array.new(9) {UNEXPLORED * 9}
+    @hidden_board = Array.new(9) {UNEXPLORED * 9}
     place_bombs
     place_numbers
+  end
+
+  def reveal(x,y) # REFACTOR OR DIE!!!
+    ints = ('1'..'8').to_a
+    hidden_square = hidden_board[x][y]
+    return false if hidden_square == BOMB
+
+    if ints.include?(hidden_square)
+      board[x][y] = hidden_square
+      return true
+    end
+
+    squares_to_reveal = [[x,y]]
+
+    until squares_to_reveal.empty?
+      x1, y1 = squares_to_reveal.shift
+      hidden_square = hidden_board[x1][y1]
+
+      board[x1][y1] = hidden_square
+
+      valid_moves(x1,y1).each do |square|
+        x2, y2 = square
+        board_square = board[x2][y2]
+        hidden_square = hidden_board[x2][y2]
+
+        next if hidden_square == BOMB
+        next unless board_square == UNEXPLORED
+
+        squares_to_reveal << [x2,y2] if hidden_square == '_'
+        board[x2][y2] = hidden_square if ints.include?(hidden_square)
+      end
+    end
+
+      # if you try to reveal a square with a number in it,
+      # you will just change the board at those coordinates to
+      # show the number
+
+    true
   end
 
   private
@@ -28,7 +67,7 @@ class Board
   def place_bombs
     10.times do |i|
       x, y = rand(9), rand(9)
-      until hidden_board[x][y] == EMPTY
+      until hidden_board[x][y] == UNEXPLORED
         x, y = rand(9), rand(9)
       end
 
@@ -65,11 +104,16 @@ class Board
       bombs += 1 if hidden_board[test_x][test_y] == BOMB
     end
 
+    return NO_BOMBS if bombs == 0
     bombs
   end
 end
 
 if $PROGRAM_NAME == __FILE__
   b = Board.new
+  #b.hidden_board = ["B211_____","12B1_____","_111111__", "11__1B1__", "B11121111", "221B1112B","B22111B21","2B1__1121","111____1B"]
   puts b.hidden_board
+  b.reveal(3,3)
+  puts
+  puts b.board
 end
